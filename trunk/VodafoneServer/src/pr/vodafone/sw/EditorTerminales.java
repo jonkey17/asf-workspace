eff4f7507425187422f60a44a49a70725521199c
hasier.rodriguez@opendeusto.es <hasier.rodriguez@opendeusto.es@4676c6c3-30c4-c75d-3f89-81aa121475a5>
2014-02-23 12:22:53 +0000

    Crear EditorTerminales y añadir los metodos a exportar en el servicio. Generar aar del servicio.

---
+++ trunk/VodafoneServer/src/pr/vodafone/sw/EditorTerminales.java
@@ -0,0 +1,87 @@
+package pr.vodafone.sw;
+
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.Vector;
+
+import pr.vodafone.dao.GestorBD;
+import pr.vodafone.dao.dto.Terminal;
+
+public class EditorTerminales {
+	
+	private GestorBD bd;
+	
+	public EditorTerminales() throws ClassNotFoundException, SQLException {
+		bd = new GestorBD();
+		bd.conectar();
+	}
+
+	public Terminal obtenerTerminal(String idTerminal) {
+		try {
+			return bd.obtenerTerminal(idTerminal);
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public Terminal[] obtenerTerminales() {
+		try {
+			Vector<Terminal> terminales = bd.obtenerTerminales();
+			Terminal[] terminalesArray = new Terminal[terminales.size()];
+			terminalesArray = terminales.toArray(terminalesArray);
+			return terminalesArray;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public Terminal[] obtenerTerminalesPorMarca(String marca) {
+		try {
+			Vector<Terminal> terminales = bd.obtenerTerminalesPorMarca(marca);
+			Terminal[] terminalesArray = new Terminal[terminales.size()];
+			terminalesArray = terminales.toArray(terminalesArray);
+			return terminalesArray;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public boolean insertarTerminal(Terminal terminal) {
+		try {
+			bd.insertarTerminal(terminal);
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+
+	public boolean actualizarTerminal(String idTerminal, Terminal terminal) {
+		try {
+			bd.actualizarTerminal(idTerminal, terminal);
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+
+	public boolean borrarTerminal(String idTerminal) {
+		try {
+			bd.borrarTerminal(idTerminal);
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+	
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		EditorTerminales e = new EditorTerminales();
+		System.out.println(Arrays.toString(e.obtenerTerminales()));
+	}
+
+}
