3840fbaf16dc9f16eb0ed1c3d10a4d1a516374e4
1rudelga@opendeusto.es <1rudelga@opendeusto.es@4676c6c3-30c4-c75d-3f89-81aa121475a5>
2014-03-21 16:40:55 +0000

    Patron singleton y metodo getBaseUri creados

---
+++ trunk/VodafoneTerminalesGUI/src/pr/vodafone/rest/gui/Singleton.java
@@ -0,0 +1,33 @@
+package pr.vodafone.rest.gui;
+
+import java.net.URI;
+
+import javax.ws.rs.core.UriBuilder;
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.api.client.config.ClientConfig;
+import com.sun.jersey.api.client.config.DefaultClientConfig;
+
+public class Singleton {
+
+	private static ClientConfig config = new DefaultClientConfig();
+	private static Client client = Client.create(config);
+	private static WebResource service = null;
+	
+	protected Singleton() {
+	 // Exists only to defeat instantiation.
+	}
+	   
+	public static WebResource getInstance() {
+	   if(service == null) {
+	      service = client.resource(getBaseURI());
+	   }
+	   return service;
+	}
+
+	public static URI getBaseURI() {
+		return UriBuilder.fromUri("http://localhost:8080/vodafone").build();
+	}
+	
+}
