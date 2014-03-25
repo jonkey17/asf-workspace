9f207ca3877e619b7d8040497ed9bcbd00ce7ab3
1rudelga@opendeusto.es <1rudelga@opendeusto.es@4676c6c3-30c4-c75d-3f89-81aa121475a5>
2014-03-25 11:10:51 +0000

    Ventana VRestClientes acabada y probada

--- VodafoneTerminalesGUI/src/pr/vodafone/rest/gui/VRestClientes.java
+++ VodafoneTerminalesGUI/src/pr/vodafone/rest/gui/VRestClientes.java
@@ -2,19 +2,30 @@ package pr.vodafone.rest.gui;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
-
+import javax.swing.SwingUtilities;
 import javax.swing.WindowConstants;
 import javax.swing.border.TitledBorder;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableModel;
-import javax.swing.SwingUtilities;
+import javax.ws.rs.core.MediaType;
+
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.UniformInterfaceException;
+
+import pr.vodafone.rest.data.Cliente;
+import pr.vodafone.rest.data.Linea;
+import pr.vodafone.rest.pattern.Singleton;
 
 /**
  * This code was edited or generated using CloudGarden's Jigloo SWT/Swing GUI
@@ -49,6 +60,7 @@ public class VRestClientes extends javax.swing.JFrame {
 	private JButton botonTodos;
 	private JTextField cajaDniBuscar;
 	private JLabel jLabel1;
+	private List<Cliente> listaActualizadaClientes;
 
 	/**
 	 * Auto-generated main method to display this JFrame
@@ -297,27 +309,132 @@ public class VRestClientes extends javax.swing.JFrame {
 	}
 
 	private void botonBuscar() {
-
+		// Get XML for application
+		try{
+			String campoDni = cajaDniBuscar.getText();
+			Cliente [] arrayClientes = Singleton.getInstance().path("rest").path("clientes").path(campoDni).accept(MediaType.APPLICATION_XML).get(Cliente[].class);
+			System.out.println("todos.GET('application/xml').results (con un ARRAY): ");
+			DefaultTableModel dtm = (DefaultTableModel) tablaClientes.getModel();
+			listaActualizadaClientes = new ArrayList<Cliente>();
+			int numFilas = dtm.getRowCount();
+			for(int k=0;k<numFilas;k++)
+				dtm.removeRow(0);
+			
+			for(int i=0;i<arrayClientes.length;i++) 
+			{
+				System.out.println(arrayClientes[i].getDni() + "\t" + arrayClientes[i].getNombre());	
+				String[]cliente = {arrayClientes[i].getDni(),arrayClientes[i].getNombre(),arrayClientes[i].getDireccion(),arrayClientes[i].getEmail()};
+				dtm.addRow(cliente);
+				listaActualizadaClientes.add(arrayClientes[i]);
+			}
+		}catch(UniformInterfaceException e){ // if code != 20x exception is thrown
+			ClientResponse r = e.getResponse();
+			System.out.println("todos.GET('application/xml').status: " + r.getStatus());
+			System.out.println("todos.GET('application/xml').entity: " + r.getEntity(String.class));
+			JOptionPane.showMessageDialog(VRestClientes.this, r.getEntity(String.class), "Error " + r.getStatus(), JOptionPane.ERROR_MESSAGE);
+		}		
 	}
 
 	private void botonTodos() {
-
+		// Get XML for application
+		try{
+			Cliente [] arrayClientes = Singleton.getInstance().path("rest").path("clientes").accept(MediaType.APPLICATION_XML).get(Cliente[].class);
+			System.out.println("todos.GET('application/xml').results (con un ARRAY): ");
+			DefaultTableModel dtm = (DefaultTableModel) tablaClientes.getModel();
+			listaActualizadaClientes = new ArrayList<Cliente>();
+			
+			int numFilas = dtm.getRowCount();
+			for(int k=0;k<numFilas;k++)
+				dtm.removeRow(0);
+			
+			for(int i=0;i<arrayClientes.length;i++) 
+			{
+				System.out.println(arrayClientes[i].getDni() + "\t" + arrayClientes[i].getNombre());	
+				String[]cliente = {arrayClientes[i].getDni(),arrayClientes[i].getNombre(),arrayClientes[i].getDireccion(),arrayClientes[i].getEmail()};
+				dtm.addRow(cliente);
+				listaActualizadaClientes.add(arrayClientes[i]);
+			}
+		}catch(UniformInterfaceException e){ // if code != 20x exception is thrown
+			ClientResponse r = e.getResponse();
+			System.out.println("todos.GET('application/xml').status: " + r.getStatus());
+			System.out.println("todos.GET('application/xml').entity: " + r.getEntity(String.class));
+			JOptionPane.showMessageDialog(VRestClientes.this, r.getEntity(String.class), "Error " + r.getStatus(), JOptionPane.ERROR_MESSAGE);
+		}		
 	}
 
 	private void botonEditar() {
+		int selectedRow = tablaClientes.getSelectedRow();
+		if (selectedRow == 0) {
+			JOptionPane.showMessageDialog(this,
+					"SELECCIONE UN TERMINAL",
+					"ATENCION", JOptionPane.WARNING_MESSAGE);
+			return;
+		}
 
+		Cliente c = listaActualizadaClientes.get(tablaClientes.getSelectedRow());
+		
+		cajaDireccion.setText(c.getDireccion());
+		cajaEmail.setText(c.getEmail());
+		cajaNombre.setText(c.getNombre());
+		cajaDni.setText(c.getDni());
 	}
 
 	private void botonVerLineas() {
-
+		Cliente clienteSeleccionado = listaActualizadaClientes.get(tablaClientes.getSelectedRow());
+		VRestLineas ventana = new VRestLineas(clienteSeleccionado);
+		System.out.println("Cliente mandado a Ibai: "+clienteSeleccionado.toString());
+		ventana.setVisible(true);
+		this.setVisible(false);
 	}
 
 	private void botonNuevo() {
+		// POST
+		Cliente c = new Cliente();
+		c.setDireccion(cajaDireccion.getText());
+		c.setDni(cajaDni.getText());
+		c.setEmail(cajaEmail.getText());
+		c.setNombre(cajaNombre.getText());
+		// TYPE is the type of content consumes by the service/method
+		// ACCEPT is the type of content returned by the service/method
+		// PUT is the type of operation invoked. The first param is the content returned and the second one the values received.
+		ClientResponse cr1 = Singleton.getInstance().path("rest").path("clientes").type(MediaType.APPLICATION_XML).accept(MediaType.APPLICATION_JSON).post(ClientResponse.class, c);
+		if (cr1.getStatus() == 201){ // Return code should be 201 == created resource
+			System.out.println("todos.POST('application/xml').status: " + cr1.getStatus());
+			System.out.println("todos.POST('application/xml').location: " + cr1.getLocation());
+			listaActualizadaClientes.add(c);
+			DefaultTableModel dtm = (DefaultTableModel)tablaClientes.getModel();
+			String[]cliente = {c.getDni(),c.getNombre(),c.getDireccion(),c.getEmail()};
+			dtm.addRow(cliente);
+		}else{ // Or code 409 == resource already exists 
+			System.out.println("todos.POST('application/xml').status: " + cr1.getStatus());
+			System.out.println("todos.POST('application/xml').entity: " + cr1.getEntity(String.class));
+			JOptionPane.showMessageDialog(VRestClientes.this, "CLIENTE EXISTENTE, NO SE HA PODIDO INSERTAR", "ATENCION", JOptionPane.ERROR_MESSAGE);
+		}
 
 	}
 
 	private void botonGuardar() {
 
+		Cliente c = new Cliente();
+		c.setDireccion(cajaDireccion.getText());
+		c.setDni(cajaDni.getText());
+		c.setEmail(cajaEmail.getText());
+		c.setNombre(cajaNombre.getText());
+		
+		try {
+			Singleton.getInstance().path("rest").path("clientes").path(c.getDni())
+					.type(MediaType.APPLICATION_XML)
+					.accept(MediaType.APPLICATION_JSON).put(c);
+			System.out.println("Linea editada correctamente");
+			Cliente c2 = listaActualizadaClientes.get(tablaClientes.getSelectedRow());
+			c2.setDireccion(c.getDireccion());
+			c2.setEmail(c.getEmail());
+			c2.setNombre(c.getNombre());
+		} catch (UniformInterfaceException e) {
+			ClientResponse r = e.getResponse();
+			JOptionPane.showMessageDialog(VRestClientes.this, r.getEntity(String.class), "Error " + r.getStatus(), JOptionPane.ERROR_MESSAGE);
+		}
+	
 	}
 
 	private void botonSalir() {
