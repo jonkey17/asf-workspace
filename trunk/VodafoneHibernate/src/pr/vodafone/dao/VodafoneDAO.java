701fa3dade8164db66fae1b1c421a7ed62075709
ibi13x@gmail.com <ibi13x@gmail.com@4676c6c3-30c4-c75d-3f89-81aa121475a5>
2014-05-05 13:59:08 +0000

    Acabadas todas las pruebas. Todo funcionando.

--- trunk/VodafoneHibernate/src/pr/vodafone/dao/VodafoneDAO.java
+++ trunk/VodafoneHibernate/src/pr/vodafone/dao/VodafoneDAO.java
@@ -52,35 +52,39 @@ public class VodafoneDAO {
 				newTDatos, newPromo, newDni));
 	}
 
-	public void activateLinea(String telf, boolean activa) {
+	public static void activateLinea(String telf, boolean activa) {
+		Lineas linea = getLinea(telf);
+		linea.setActiva(activa);
 
+		LineaFacade lineaFacade = new LineaFacade();
+		lineaFacade.edit(linea);
 	}
 
-	public Clientes getCliente(String dni) {
+	public static Clientes getCliente(String dni) {
 		ClienteFacade clienteFacade = new ClienteFacade();
 		Clientes cliente = clienteFacade.find(dni);
 		return cliente;
 	}
 
-	public List<Clientes> getClientes() {
+	public static List<Clientes> getClientes() {
 		ClienteFacade clienteFacade = new ClienteFacade();
 		List<Clientes> listaClientes = clienteFacade.findAll();
 		return listaClientes;
 	}
 
-	public void updateCliente(String dni, String newNombre, String newDir,
-			String newEmail) {
+	public static void updateCliente(String dni, String newNombre,
+			String newDir, String newEmail) {
 		ClienteFacade clienteFacade = new ClienteFacade();
 		clienteFacade.edit(new Clientes(dni, newNombre, newDir, newEmail));
 	}
 
-	public Facturas getFactura(int id) {
+	public static Facturas getFactura(int id) {
 		FacturaFacade facturaFacade = new FacturaFacade();
 		Facturas factura = facturaFacade.find(id);
 		return factura;
 	}
 
-	public List<Facturas> getFacturasByLinea(String telf) {
+	public static List<Facturas> getFacturasByLinea(String telf) {
 		FacturaFacade facturaFacade = new FacturaFacade();
 		List<Facturas> listaFacturas = facturaFacade.findAllByLinea(telf);
 		return listaFacturas;
@@ -88,40 +92,81 @@ public class VodafoneDAO {
 
 	public static void main(String[] args) {
 
-		System.out.println("Prueba m�todo getLineas()");
+		System.out.println();
+		System.out.println("PRUEBAS DE LAS LINEAS");
+		System.out.println("-----------------------");
+		System.out.println();
+		
+		System.out.println("Prueba metodo getLineas()");
 		List<Lineas> listaLineas = getLineas();
 		for (int k = 0; k < listaLineas.size(); k++)
 			System.out.println(listaLineas.get(k));
 
-		System.out.println("\n Prueba m�todo getLinea(666333000) ");
+		System.out.println("Prueba metodo getLinea(666333000)");
 		Lineas linea = getLinea("666333000");
 		System.out.println(linea);
 
-		System.out.println("\n Prueba m�todo getLineasByEstado(true) ");
+		System.out.println("Prueba metodo getLineasByEstado(true)");
 		List<Lineas> listaLineasByEstado = getLineasByEstado(true);
 		for (int k = 0; k < listaLineasByEstado.size(); k++)
 			System.out.println(listaLineasByEstado.get(k));
 
-		System.out.println("\n Prueba m�todo getLineasByCliente(111) ");
+		System.out.println("Prueba metodo getLineasByCliente(111)");
 		List<Lineas> listaLineasByCliente = getLineasByCliente("111");
 		for (int k = 0; k < listaLineasByCliente.size(); k++)
 			System.out.println(listaLineasByCliente.get(k));
 
 		System.out
-				.println("\n Prueba m�todo getLineasByClienteAndEstado(111,true) ");
+				.println("Prueba metodo getLineasByClienteAndEstado(111,true)");
 		List<Lineas> listaLineasByClienteAndEstado = getLineasByClienteAndEstado(
 				"111", true);
 		for (int k = 0; k < listaLineasByClienteAndEstado.size(); k++)
 			System.out.println(listaLineasByClienteAndEstado.get(k));
 
 		System.out
-				.println("\n Prueba m�todo updateLinea(666111000, 23-12-1991, false, Ninguna ");
-		// updateLinea("666111000", "23-01-1992", true, "Ninguna", "Si",
-		// "Consumo", "111");
+				.println("Prueba metodo updateLinea(666111000, 23-12-1991, false, Ninguna ");
 		System.out.println("Linea sin actualizar " + getLinea("666111000"));
 		updateLinea("666111000", "23-12-1991", false, "Ninguna", "No",
 				"Consumo", "111");
 		System.out.println("Linea actualizada " + getLinea("666111000"));
+
+		System.out.println("Prueba metodo activateLinea");
+		System.out.println("Estado linea: " + getLinea("666111000").isActiva());
+		activateLinea("666111000", true);
+		System.out.println("Estado linea: " + getLinea("666111000").isActiva());
+		
+		System.out.println();
+		System.out.println("PRUEBAS DE LOS CLIENTES");
+		System.out.println("-----------------------");
+		System.out.println();
+		
+		System.out.println("Prueba metodo getCliente");
+		System.out.println(getCliente("111").getNombre());
+
+		System.out.println("Prueba metodo getClientes");
+		List<Clientes> clientes = getClientes();
+		for (Clientes c : clientes) {
+			System.out.println("Cliente: " + c.getNombre());
+		}
+
+		System.out.println("Prueba metodo updateCliente");
+		System.out.println(getCliente("111").getNombre());
+		updateCliente("111", "Nuevo nombre", "Dir", "Mail");
+		System.out.println(getCliente("111").getNombre());
+		
+		System.out.println();
+		System.out.println("PRUEBAS DE LAS FACTURAS");
+		System.out.println("-----------------------");
+		System.out.println();
+
+		System.out.println("Prueba metodo getFactura");
+		System.out.println(getFactura(1).getImporte());
+
+		System.out.println("Prueba metodo getFacturasByLinea");
+		List<Facturas> facturas = getFacturasByLinea("666111000");
+		for (Facturas f : facturas) {
+			System.out.println("ID linea: " + f.getIdFactura());
+		}
 
 		Singleton.close();
 
